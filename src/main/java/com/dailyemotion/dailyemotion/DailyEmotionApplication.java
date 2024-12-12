12574641041ef2c508bd95b37126b6f3e3c0c207
Soo186 <rlagustn9797@gmail.com>
2024-12-12 13:56:38 +0900

    chore: Add initial dependencies

---
+++ src/main/java/com/dailyemotion/dailyemotion/DailyEmotionApplication.java
@@ -0,0 +1,13 @@
+package com.dailyemotion.dailyemotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyEmotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyEmotionApplication.class, args);
+	}
+
+}
