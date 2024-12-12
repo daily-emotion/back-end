d8f7129f0d9437f86940453d14f8809a33cfa4de
Soo186 <161421422+Soo186@users.noreply.github.com>
2024-12-12 14:00:31 +0900

    chore: Add initial dependencies (#1)

---
+++ src/test/java/com/dailyemotion/dailyemotion/DailyEmotionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailyemotion.dailyemotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyEmotionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
