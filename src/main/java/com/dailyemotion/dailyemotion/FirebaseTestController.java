bafa3aa89d0dc05d04e7b8f5fa8ec8ea51b2b4cb
Soo186 <161421422+Soo186@users.noreply.github.com>
2024-12-14 01:12:35 +0900

    Chore : Firebase 설정 및 연결 확인 (#4)
    * chore: firebase setting
    
    -Spring Security 의존성 주석 처리
    -Firebase 의존성 추가
    
    * chore: firebase setting
    
    -Spring context path /api 추가 (라우팅 경로랑 겹치치않기 위해)
    -Firebase 파일 설정 분리
    
    * chore: firebase setting
    
    -firebaseServiceAccounntKey ignore
    
    * chore: firebase setting
    
    -firebase 테스트 컨트롤러
    
    * chore: firebase setting
    
    -firebase config

---
+++ src/main/java/com/dailyemotion/dailyemotion/FirebaseTestController.java
@@ -0,0 +1,32 @@
+package com.dailyemotion.dailyemotion;
+
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.cloud.FirestoreClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/firebase/test")
+public class FirebaseTestController {
+
+    @GetMapping("/connection")
+    public ResponseEntity<String> testConnection() {
+        try {
+            // Firebase 앱이 초기화되었는지 확인
+            FirebaseApp app = FirebaseApp.getInstance();
+
+            // Firestore 데이터베이스 참조 가져오기 (간단한 테스트용)
+            Firestore db = FirestoreClient.getFirestore();
+
+            return ResponseEntity.ok("Firebase 연결 성공! App name: " + app.getName());
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Firebase 연결 실패: " + e.getMessage());
+        }
+    }
+}
