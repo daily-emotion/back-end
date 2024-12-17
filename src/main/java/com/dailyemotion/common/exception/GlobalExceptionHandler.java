052252b878e8d1874a762e806c149e1bb33427ca
pangwoo96 <junggw96@gmail.com>
2024-12-17 16:59:06 +0900

    reafactor : 예외 핸들러 수정 - UserException 추가

--- src/main/java/com/dailyemotion/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/dailyemotion/common/exception/GlobalExceptionHandler.java
@@ -1,17 +1,31 @@
 package com.dailyemotion.common.exception;
 
 import com.dailyemotion.common.errorCode.DiaryErrorCode;
+import com.dailyemotion.common.errorCode.UserErrorCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.View;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    private final View error;
+
+    public GlobalExceptionHandler(View error) {
+        this.error = error;
+    }
     // 발생한 예외를 캐치하고 상태코드와 메시지를 클라이언트에게 전달
 
     @ExceptionHandler(DiaryException.class)
     public ResponseEntity<String> handleDiaryException(DiaryException ex) {
         DiaryErrorCode errorCode = ex.getErrorCode();
+        return ResponseEntity.status(errorCode.getStatus())
+                .body(errorCode.getMessage());
+    }
+
+    @ExceptionHandler(UserException.class)
+    public ResponseEntity<String> handleUserException(UserException ex) {
+        UserErrorCode errorCode = ex.getErrorCode();
         return ResponseEntity.status(errorCode.getStatus())
                 .body(errorCode.getMessage());
     }
