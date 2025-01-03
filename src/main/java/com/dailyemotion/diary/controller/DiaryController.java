2ff66bc72a9da3e7e7790839ae831a78775da6ac
pangwoo96 <junggw96@gmail.com>
2025-01-04 04:16:20 +0900

    feat : diary-get (월간) 기능 구현 - Controller 구현 - Service, Repository 구현 - 단위 테스트 코드 구현

--- src/main/java/com/dailyemotion/diary/controller/DiaryController.java
+++ src/main/java/com/dailyemotion/diary/controller/DiaryController.java
@@ -1,6 +1,7 @@
 package com.dailyemotion.diary.controller;
 
 import com.dailyemotion.diary.dto.request.DiaryReqDto;
+import com.dailyemotion.diary.dto.response.DiaryGetResDto;
 import com.dailyemotion.diary.dto.response.DiaryResDto;
 import com.dailyemotion.diary.service.DiaryService;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,6 +19,7 @@ public class DiaryController {
 
     private final DiaryService diaryService;
 
+    // 다이어리 생성
     @PostMapping("/{date}")
     public ResponseEntity<DiaryResDto> createDiary(@PathVariable(name = "date") LocalDate date,
                                                    @RequestBody DiaryReqDto diaryReqDto) {
@@ -24,22 +27,32 @@ public class DiaryController {
         return ResponseEntity.status(HttpStatus.CREATED).body(diaryResDto);
     }
 
+    // 다이어리 삭제
     @DeleteMapping("/{date}")
     public ResponseEntity<Void> deleteDiary(@PathVariable(name = "date") LocalDate date) {
         diaryService.deleteDiary(date);
         return ResponseEntity.noContent().build();
     }
 
+    // 다이어리 조회 (일간)
     @GetMapping("/{date}")
     public ResponseEntity<DiaryResDto> getDiary(@PathVariable(name = "date") LocalDate date) {
         DiaryResDto diaryResDto = diaryService.getDiary(date);
         return ResponseEntity.status(HttpStatus.OK).body(diaryResDto);
     }
 
+    // 다이어리 수정
     @PutMapping("/{date}")
     public ResponseEntity<DiaryResDto> updateDiary(@PathVariable(name = "date") LocalDate date,
                                                    @RequestBody DiaryReqDto diaryReqDto) {
         DiaryResDto diaryResDto = diaryService.updateDiary(date, diaryReqDto);
         return ResponseEntity.status(HttpStatus.OK).body(diaryResDto);
+    }
+
+    // 다이어리 조회 (월간)
+    @GetMapping("/monthly/{month}")
+    public ResponseEntity<List<DiaryGetResDto>> getMonthlyDiary(@PathVariable(name = "month") String month) {
+        List<DiaryGetResDto> diaryGetResDto = diaryService.getMonthlyDiary(month);
+        return ResponseEntity.status(HttpStatus.OK).body(diaryGetResDto);
     }
 }
--- src/main/java/com/dailyemotion/domain/repository/DiaryRepository.java
+++ src/main/java/com/dailyemotion/domain/repository/DiaryRepository.java
@@ -2,9 +2,12 @@ package com.dailyemotion.domain.repository;
 
 import com.dailyemotion.domain.entity.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
@@ -12,4 +15,7 @@ public interface DiaryRepository extends JpaRepository<Diary, Long> {
     boolean existsByDate(LocalDate date);
 
     Diary findByDate(LocalDate date);
+
+    @Query("SELECT d FROM Diary d WHERE d.date BETWEEN :startDate AND :endDate")
+    List<Diary> findByDateBetween(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 }
