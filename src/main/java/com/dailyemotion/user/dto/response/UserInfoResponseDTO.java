afb1c703eb64b79fe6c30cc2e7af4a88cdd04d90
Soo186 <rlagustn9797@gmail.com>
2025-01-04 20:20:57 +0900

    chore : repsontDto 설정 (추후 수정 예정)

---
+++ src/main/java/com/dailyemotion/user/dto/response/UserInfoResponseDTO.java
@@ -0,0 +1,15 @@
+package com.dailyemotion.user.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserInfoResponseDTO {
+    private String name;
+    private String role;
+}
