5719c32adb6e76f5682d8e56ec7b6a026177b858
pangwoo96 <junggw96@gmail.com>
2025-01-21 15:36:55 +0900

    refactor : 개발환경, docker 서버 분리 - DB URL 주소 환경변수 설정 - 오타 수정 (@Vaild -> @Valid) - 오타 수정 (UserException -> TagException) - 서버 컨테이너 설정 (docker-compose) - 애플리케이션 .jar로 copy 설정 (dockerfile)

--- src/test/java/com/dailyemotion/dailyemotion/diary/controller/DiaryControllerTest.java
+++ src/test/java/com/dailyemotion/dailyemotion/diary/controller/DiaryControllerTest.java
@@ -15,9 +15,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
@@ -37,10 +37,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @TestMethodOrder(OrderAnnotation.class)
 public class DiaryControllerTest {
 
-    @MockitoBean
+    @MockBean
     private DiaryService diaryService;
 
-    @MockitoBean
+    @MockBean
     private TagService tagService;
 
     @Autowired
