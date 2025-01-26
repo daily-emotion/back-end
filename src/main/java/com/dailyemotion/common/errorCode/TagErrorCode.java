3ade264104789aff74e74f71a388710c3ee6fda4
Soo186 <rlagustn9797@gmail.com>
2025-01-26 18:32:05 +0900

    feat: TagErrorCode

--- src/main/java/com/dailyemotion/common/errorCode/TagErrorCode.java
+++ src/main/java/com/dailyemotion/common/errorCode/TagErrorCode.java
@@ -7,8 +7,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum TagErrorCode {
-    // 에러메시지와 상태코드(HttpStatus)를 관리
-    TAG_NOT_FOUND("태그를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+    TAG_COUNT_EXCEEDED("태그는 최대 3개까지만 선택할 수 있습니다.", HttpStatus.BAD_REQUEST),
+    INVALID_TAG_NAME("유효하지 않은 태그입니다.", HttpStatus.BAD_REQUEST);
+
 
     private final String message;
     private final HttpStatus status;
