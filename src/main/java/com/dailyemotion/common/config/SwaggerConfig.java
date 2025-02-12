a3895b8718ed79cd5f19023a3db19c231a994ce3
Soo186 <rlagustn9797@gmail.com>
2025-02-12 22:43:31 +0900

    refactor:  swagger 경로 https로 수정

--- src/main/java/com/dailyemotion/common/config/SwaggerConfig.java
+++ src/main/java/com/dailyemotion/common/config/SwaggerConfig.java
@@ -1,10 +1,11 @@
 package com.dailyemotion.common.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +18,8 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         String jwtSchemeName = "Authorization";
+
+        // JWT 보안 설정
         SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
         Components components = new Components()
                 .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
@@ -29,11 +32,14 @@ public class SwaggerConfig {
                 .components(components)
                 .security(List.of(securityRequirement))
                 .info(apiInfo())
-                .tags(List.of(          // 태그 순서 지정
-                        new io.swagger.v3.oas.models.tags.Tag().name("1. User Controller").description("User API"),
-                        new io.swagger.v3.oas.models.tags.Tag().name("2. Diary Controller").description("Diary API"),
+                .servers(List.of(  // 서버 URL 순서 지정
+                        new Server().url("https://dailymotion.site/api").description("Production Server"),
+                        new Server().url("http://localhost:8080/api").description("Local Server")
+                ))
+                .tags(List.of(  // 태그 순서 지정
+                        new Tag().name("1. User Controller").description("User API"),
+                        new Tag().name("2. Diary Controller").description("Diary API"),
                         new Tag().name("3. Statistics Controller").description("Statistics API")
-
                 ));
     }
 
