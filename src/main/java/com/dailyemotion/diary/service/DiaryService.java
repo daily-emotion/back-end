f7dff86d98829cbaf058c52f4b28c22c9941bb6c
pangwoo96 <junggw96@gmail.com>
2025-03-17 12:41:52 +0900

    refactor : cache 반영 수정 - 병합 해결

--- src/main/java/com/dailyemotion/diary/service/DiaryService.java
+++ src/main/java/com/dailyemotion/diary/service/DiaryService.java
@@ -3,6 +3,7 @@ package com.dailyemotion.diary.service;
 import com.dailyemotion.common.exception.DiaryException;
 import com.dailyemotion.common.exception.TagException;
 import com.dailyemotion.common.exception.UserException;
+import com.dailyemotion.common.utill.SecurityUtils;
 import com.dailyemotion.common.utill.SecurityUtilsUsername;
 import com.dailyemotion.diary.dto.request.DiaryReqDto;
 import com.dailyemotion.diary.dto.response.DiaryGetResDto;
@@ -14,6 +15,7 @@ import com.dailyemotion.domain.entity.User;
 import com.dailyemotion.tag.repository.TagRepository;
 import com.dailyemotion.tag.service.TagService;
 import com.dailyemotion.user.repository.UserRepository;
+import com.fasterxml.jackson.core.type.TypeReference;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -43,6 +45,7 @@ public class DiaryService {
     private final TagService tagService;
     private final TagRepository tagRepository;
     private final ImageService imageService;
+    private final DiaryCacheService diaryCacheService;
 
     // 다이어리 생성
     public DiaryResDto createDiary(LocalDate date, DiaryReqDto diaryReqDto) {
@@ -105,7 +108,14 @@ public class DiaryService {
     // 월별 다이어리 조회
     public List<DiaryGetResDto> getMonthlyDiary(String month) {
         validateDiaryCreation(month);
-        String username = SecurityUtilsUsername.getCustomOAuth2UserName();
+        String username = SecurityUtils.getCustomOAuth2User();
+        String cacheKey = "cache:getMonthlyDiaries:" + username + ":" + month;
+
+        // 캐시가 있으면 반환
+        List<DiaryGetResDto> cachedData = diaryCacheService.getCache(cacheKey, new TypeReference<List<DiaryGetResDto>>() {});
+        if (cachedData != null) {
+            return cachedData;
+        }
 
         LocalDate targetMonthStart = LocalDate.parse(month + "01", DateTimeFormatter.ofPattern("yyyyMMdd"));
         LocalDate startDate = targetMonthStart.minusMonths(1).withDayOfMonth(1);
@@ -120,9 +130,14 @@ public class DiaryService {
             return Collections.emptyList();
         }
 
-        return diaries.stream()
+        List<DiaryGetResDto> diaryGetResDto = diaries.stream()
                 .map(DiaryGetResDto::from)
                 .collect(Collectors.toList());
+
+        // 캐시에 저장
+        diaryCacheService.setCache(cacheKey, diaryGetResDto);
+
+        return diaryGetResDto;
     }
 
 
